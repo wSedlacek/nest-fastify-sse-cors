f703600d1f12c9259b753ef518f71c943cfc37b0
wSedlacek <wsedlacekc@gmail.com>
2021-11-29 03:19:15 -0800

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
